25bec1eda9de7ce0054cfd63ee2ad49daba22e98
piela.bartek@gmail.com <piela.bartek@gmail.com>
2017-09-20 13:39:52 +0200

    Person class

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php 
 
 require 'car.php';
+require 'person.php';
 
 $car = new Car;
 $car->setBrand('Audi');
@@ -16,5 +17,11 @@ $car2->setType('kombi');
 $car2->setProductionDate(2012);
 $car2->setOwner('Adam');
 
+
+$kasia = new Person;
+$kasia->setName('Kasia');
+$kasia->setBirthDate(2005);
+$kasia->setBirthPlace('Zywiec');
+
 echo '<pre>';
-var_dump($car);
+var_dump($kasia);
--- person.php
+++ person.php
@@ -6,6 +6,15 @@ class Person {
     private $birthDate = null;
     private $birthPlace = null;
 
-    
+    public function setName($name){
+        $this->name = $name;
+    }
 
+    public function setBirthDate($date){
+        $this->birthDate = $date;
+    }
+
+    public function setBirthPlace($place){
+        $this->birthPlace = $place;
+    }
 }
